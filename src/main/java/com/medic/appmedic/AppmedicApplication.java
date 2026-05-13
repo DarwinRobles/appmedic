2b169a79090338368731272613a528865270e2a7
Darwin <darwinroblesreb@gmail.com>
2026-05-13 14:10:24 -0500

    Initial project app medic

---
+++ src/main/java/com/medic/appmedic/AppmedicApplication.java
@@ -0,0 +1,13 @@
+package com.medic.appmedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmedicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medic/appmedic/AppmedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medic.appmedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
